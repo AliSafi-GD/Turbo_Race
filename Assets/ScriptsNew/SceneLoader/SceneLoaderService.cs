ed8b6d8fcca76d59163eae3a71c7fb3be19b7b4e
ali safi <ali800safi@gmail.com>
2024-09-15 21:15:18 +0330

    add service locator and singletone structure

--- Assets/ScriptsNew/SceneLoader/SceneLoaderService.cs
+++ Assets/ScriptsNew/SceneLoader/SceneLoaderService.cs
@@ -1,18 +1,90 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Services;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneLoaderService : MonoBehaviour
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static T _instance;
+    
+    public static T Instance
     {
-        
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                if (_instance == null)
+                {
+                    GameObject singletonObject = new GameObject();
+                    _instance = singletonObject.AddComponent<T>();
+                    singletonObject.name = typeof(T).ToString() + " (Singleton)";
+                    DontDestroyOnLoad(singletonObject);
+                }
+            }
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Awake()
     {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
+
+
+public class SceneLoaderService : MonoBehaviour, IService
+{
+    public event Action<float> OnProgressChanged;
+    public event Action OnCompleted;
+    readonly string sceneLoaderName = "SceneLoader";
+    private string currentSceneName;
+    private string nextSceneName;
+    
+    public void LoadNewScene(string nextSceneName)
+    {
+        currentSceneName = SceneManager.GetActiveScene().name;
+        this.nextSceneName = nextSceneName;
+
+        StartCoroutine(IE_LoadNewScene());
+    }
+
+    IEnumerator IE_LoadNewScene()
+    {
+        var loadingScene = SceneManager.LoadSceneAsync(sceneLoaderName,LoadSceneMode.Additive);
+        while (!loadingScene.isDone)
+        {
+            yield return null;
+        }
+
+        var previousScene = SceneManager.UnloadSceneAsync(currentSceneName);
+        while (!previousScene.isDone)
+        {
+            yield return null;
+        }
         
+        var nextScene = SceneManager.LoadSceneAsync(nextSceneName,LoadSceneMode.Additive);
+        while (!nextScene.isDone)
+        {
+            yield return null;
+        }
+
+        loadingScene = SceneManager.UnloadSceneAsync(sceneLoaderName);
+        while (!loadingScene.isDone)
+        {
+            yield return null;
+        }
+        OnCompleted?.Invoke();
     }
 }
---
+++ Assets/ScriptsNew/ServicesLocator/ServiceLocator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Services
+{
+   
+    public interface IService { }
+
+    public class ServiceLocator
+    {
+        private static ServiceLocator _instance;
+        private Dictionary<Type, IService> _services;
+
+        private ServiceLocator()
+        {
+            _services = new Dictionary<Type, IService>();
+        }
+
+        public static ServiceLocator Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ServiceLocator();
+                }
+                return _instance;
+            }
+        }
+
+        public void RegisterService<T>(T service) where T : IService
+        {
+            _services[typeof(T)] = service;
+        }
+
+        public T GetService<T>() where T : IService
+        {
+            if (_services.TryGetValue(typeof(T), out IService service))
+            {
+                return (T)service;
+            }
+            throw new Exception($"Service of type {typeof(T)} not registered.");
+        }
+
+        public void UnregisterService<T>() where T : IService
+        {
+            _services.Remove(typeof(T));
+        }
+    }
+}
